46e7b660abba97c94a84022d8f8a7bf7e3cb96a1
agolautner <lautner.agoston@gmail.com>
2022-01-05 20:59:44 +0100

    8. function vs class components

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//THERE ARE TWO WAYS TO WRITE COMPONENTS:
+//using a CLASS and using a FUNCTION
+
 class Hello extends React.Component {
     render() {
         return (
@@ -9,5 +12,20 @@ class Hello extends React.Component {
         );
     }
 }
+
+//historically, function components couldn't use important features such as STATE and LIFECYCLE METHODS
+//with the introduction of HOOKS, we can now write full-featured function components (this course covers hooks!)
+
+/* function Hello() {
+    return (
+        <div>
+            <h1>Hello there!</h1>
+            <h1>Hello there!</h1>
+            <h1>Hello there!</h1>
+        </div>
+    );
+} */
+
+//NOTE THAT THE CLASS HAS A SEPARATE RENDER METHOD, WHILE THE FUNCTION DOES NOT
 
 ReactDOM.render(<Hello/>, document.getElementById('root'));
